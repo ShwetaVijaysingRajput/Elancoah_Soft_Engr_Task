9656e19fc4961652e2f3fea9e0cd221952d0955f
ShwetaVijaysingRajput <134722514+ShwetaVijaysingRajput@users.noreply.github.com>
2023-06-05 11:30:27 +0530

    Home page (#17)
    * deleted eslintrc unused files
    
    * added pages
    
    * Update eslint.yml

--- elanco/src/routes/index.tsx
+++ elanco/src/routes/index.tsx
@@ -1,8 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout";
+import Home from "../modules/home";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "applications",
+        element: <Home />,
+      },
+    ],
   },
 ]);
