97cf17fd656b7f8501121bc6800495d22765d8a5
ShwetaVijaysingRajput <134722514+ShwetaVijaysingRajput@users.noreply.github.com>
2023-06-10 22:55:32 +0530

    Home Page comp integrated (#20)
    Added AG Grid table to home page

--- elanco/src/services/axios-instance/index.ts
+++ elanco/src/services/axios-instance/index.ts
@@ -4,7 +4,6 @@ import { apiUrls } from "../../utils/api-constants";
 export default function apiClient() {
   const instance = axios.create({
     baseURL: apiUrls.baseUrl,
-    timeout: 5000,
   });
   return instance;
 }
